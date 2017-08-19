49fdb888513d9b471e3ada487d0128fbf8b40679
Achint Sandhu <sandhu@7theta.com>
2017-08-19 07:46:42 -0400

    Support advanced builds

--- src/leiningen/new/re_frame_7theta/prod/clj/server.clj
+++ src/leiningen/new/re_frame_7theta/prod/clj/server.clj
@@ -1,10 +1,12 @@
 (ns {{ns-name}}.server
-    (:require [{{ns-name}}.app :refer [app]]
+    (:require [{{ns-name}}.config :refer [config]]
               [{{ns-name}}.handler :refer [handler]]
               [config.core :refer [env]]
-              [org.httpkit.server :refer [run-server]])
+              [org.httpkit.server :refer [run-server]]
+              [integrant.core :as ig])
     (:gen-class))
 
 (defn -main [& args]
-  (let [port (Integer/parseInt (or (env :port) "8080"))]
+  (let [port (Integer/parseInt (or (env :port) "8080"))
+        app (ig/init config)]
     (run-server (handler (:via.server.client-proxy/client-proxy app)) {:port port})))
--- src/leiningen/new/re_frame_7theta/project.clj
+++ src/leiningen/new/re_frame_7theta/project.clj
@@ -1,9 +1,9 @@
 (defproject {{ns-name}} "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.854"]
+                 [org.clojure/clojurescript "1.9.904"]
 
                  [reagent "0.7.0"]
-                 [re-frame "0.9.4"]{{#garden?}}
+                 [re-frame "0.10.1"]{{#garden?}}
                  [garden "1.3.2"]{{/garden?}}{{#server?}}
 
                  [http-kit "2.2.0"]
@@ -12,9 +12,9 @@
                  [ring/ring-anti-forgery "1.1.0"]
                  [compojure "1.6.0"]{{#via?}}
 
-                 [com.7theta/re-frame-via-fx "0.2.3"]{{/via?}}
+                 [com.7theta/re-frame-via-fx "0.2.4"]{{/via?}}
 
-                 [integrant "0.5.0"]
+                 [integrant "0.6.0"]
                  [yogthos/config "0.8"]{{/server?}}]
   :min-lein-version "2.5.3"{{#server?}}
   :source-paths ["src/clj"]{{/server?}}
@@ -26,18 +26,19 @@
                    :dependencies [{{#re-frisk?}}[re-frisk "0.4.5"]
                                   {{/re-frisk?}}[ns-tracker "0.3.1"]
                                   [binaryage/devtools "0.9.4"]
-                                  [figwheel-sidecar "0.5.12"]
+                                  [figwheel-sidecar "0.5.13"]
                                   [com.cemerick/piggieback "0.2.2"]{{#server?}}
                                   [ring/ring-devel "1.6.2"]
                                   [integrant/repl "0.2.0"]{{/server?}}]
                    :plugins [[lein-cljsbuild "1.1.7" :exclusions [org.apache.commons/commons-compress]]{{#garden?}}
                              [lein-garden "0.3.0" :exclusions [org.clojure/clojure]]{{/garden?}}{{#less?}}
                              [lein-less "1.7.5"]{{/less?}}
-                             [lein-figwheel "0.5.12" :exclusions [org.clojure/clojure]]{{#test?}}
+                             [lein-figwheel "0.5.13" :exclusions [org.clojure/clojure]]{{#test?}}
                              [lein-doo "0.1.7"]{{/test?}}]}{{#server?}}
              :uberjar {:source-paths ["prod/clj"]
                        :main {{name}}.server
                        :aot [{{name}}.server]
+                       :prep-tasks [["cljsbuild" "once" "min"]]
                        :uberjar-name "{{name}}.jar"}{{/server?}}}
   :cljsbuild {:builds [{:id "dev"
                         :source-paths ["src/cljs" "dev/cljs"]
@@ -69,5 +70,5 @@
                                             :pretty-print? true}}]}{{/garden?}}{{#less?}}
   :less {:source-paths ["less"]
          :target-path "resources/public/css"}
-  {{/less?}}:prep-tasks [["cljsbuild" "once" "min"]{{#garden?}} ["garden" "once"] {{/garden?}}{{#less?}}
+  {{/less?}}:prep-tasks [{{#garden?}} ["garden" "once"] {{/garden?}}{{#less?}}
                          ["less" "once"]{{/less?}} "compile"])
