bc136cf7d213f9ef387cc3ebe89895b4cc1f176b
Achint Sandhu <sandhu@7theta.com>
2018-01-21 14:25:21 -0500

    Upgrade dependencies

--- src/leiningen/new/re_frame_7theta/project.clj
+++ src/leiningen/new/re_frame_7theta/project.clj
@@ -14,24 +14,22 @@
                  [ring/ring-anti-forgery "1.1.0"]
                  [compojure "1.6.0"]{{#via?}}
 
-                 [com.7theta/re-frame-via-fx "0.2.10"]{{/via?}}{{#reflecti?}}
+                 [com.7theta/re-frame-via-fx "0.2.11"]{{/via?}}{{#reflecti?}}
                  [com.7theta/reflecti "1.2.1"]{{/reflecti?}}
 
-                 [integrant "0.6.2"]
-                 [yogthos/config "0.9"]{{/server?}}]
-  :min-lein-version "2.5.3"{{#server?}}
+                 [integrant "0.6.3"]
+                 [yogthos/config "0.9"]{{/server?}}]{{#server?}}
   :source-paths ["src/clj"]{{/server?}}
-  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"{{#test?}}
+  :clean-targets ^{:protect false} ["target" "resources/public/js/compiled"{{#test?}}
                                     "test/js"{{/test?}}{{#garden?}}
                                     "resources/public/css"{{/garden?}}]
-  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
   :profiles {:dev {:source-paths ["dev/clj"]
-                   :dependencies [{{#trace?}}[day8.re-frame/trace "0.1.13"]
-                                  {{/trace?}}[ns-tracker "0.3.1"]
-                                  [binaryage/devtools "0.9.8"]
+                   :dependencies [[binaryage/devtools "0.9.8"]
                                   [figwheel-sidecar "0.5.14"]
                                   [com.cemerick/piggieback "0.2.2"]{{#server?}}
-                                  [integrant/repl "0.2.0"]{{/server?}}]
+                                  [integrant/repl "0.3.0"]{{/server?}}{{#trace?}}
+                                  [day8.re-frame/trace "0.1.13"]{{/trace?}}]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
                    :plugins [[lein-cljsbuild "1.1.7" :exclusions [org.apache.commons/commons-compress]]{{#garden?}}
                              [lein-garden "0.3.0" :exclusions [org.clojure/clojure]]{{/garden?}}{{#less?}}
                              [lein-less "1.7.5"]{{/less?}}
@@ -39,7 +37,7 @@
                              [lein-doo "0.1.7"]{{/test?}}]}{{#server?}}
              :uberjar {:source-paths ["prod/clj"]
                        :main {{name}}.server
-                       :aot [{{name}}.server]
+                       :aot :all
                        :prep-tasks [["cljsbuild" "once" "min"]]
                        :uberjar-name "{{name}}.jar"}{{/server?}}}
   :cljsbuild {:builds [{:id "dev"
